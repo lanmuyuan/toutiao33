f357dafeeda5137053f6f3b1c5ebae7f3a24c39a
蓝慕远 <10488913+lan-muyuan@user.noreply.gitee.com>
2022-09-06 18:12:31 +0800

    feat:登录页完成一半

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <i class="toutiao toutiao-gengduo"></i>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="box"></div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -24,10 +18,4 @@ export default {
 }
 </script>
 
-<style scoped>
-.box {
-  background-color: red;
-  width: 700px;
-  height: 88px;
-}
-</style>
+<style scoped></style>
